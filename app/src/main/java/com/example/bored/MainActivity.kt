1bf06e62681fed6cd45994b53ee5873f4493b338
Kshitij Kumar <kshitij2212@gmail.com>
2021-04-14 23:36:27 +0530

    ui complete

--- app/src/main/java/com/example/bored/MainActivity.kt
+++ app/src/main/java/com/example/bored/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.bored
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.example.bored.databinding.ActivityMainBinding
 import com.example.bored.utils.Result
@@ -20,29 +20,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupViews()
         observeLiveData()
+    }
+
+    private fun setupViews() {
+        binding.btnGetRandom.setOnClickListener {
+            viewmodel.getRandomActivity()
+        }
     }
 
     private fun observeLiveData() {
         viewmodel.uiState.observe(this) {
             when(it) {
                 is Result.Idle -> {
-                    Log.d("MainActivity", "Idle")
-
-                }//stop loading
+                    binding.progressBar.visibility = View.GONE
+                }
                 is Result.Error -> {
                     Toast.makeText(this, it.errorMsg, Toast.LENGTH_SHORT).show()
                 }
                 is Result.Loading -> {
-                    Log.d("MainActivity", "Loading")
-
-                }//show loading
+                    binding.progressBar.visibility = View.VISIBLE
+                }
             }
         }
 
         viewmodel.activity.observe(this) {
             //show the response in the area
-            Log.d("MainActivity", "The activity is : $it")
+            binding.apply {
+                tvActivity.text = it.activity
+                tvAccess.text = it.accessibility.toString()
+                tvType.text = it.type
+                tvPrice.text = it.price.toString()
+            }
         }
     }
 
